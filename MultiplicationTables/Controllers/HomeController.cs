ac00304199874463f51369494ba676a7b861b2e6
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2020-05-15 20:17:45 -0500

    first commit

---
+++ MultiplicationTables/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MultiplicationTables.Models;
+
+namespace MultiplicationTables.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(MultiplyTableListVM model )
+        {
+            var vm = new MultiplyTableListVM();
+            vm.MultiplyTableList = new List<MultiplyTable>();
+
+            for (int iterator = 1; iterator <= 10; iterator++)
+            {
+                vm.MultiplyTableList.Add(new MultiplyTable {multiplying = iterator, result = (model.numberTable * iterator) });
+            }
+            ViewBag.numberTable = model.numberTable;
+            ViewBag.showTable = true;
+
+            return View("Index", vm);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MultiplicationTables/Models/MultiplicationTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultiplicationTables.Models
+
+{
+    public class MultiplyTable
+    {
+        public int multiplying { get; set; }
+        public int result { get; set; }
+    }
+
+    public class MultiplyTableListVM
+    {
+        public int numberTable { get; set; }
+        public IList<MultiplyTable> MultiplyTableList { get; set; }
+    }
+}
